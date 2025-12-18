ef3e6eb19491e8ff6511fc53b357c5aaf3a5f5fa
Tatytyta <sandra14taty@gmail.com>
2025-12-17 19:58:15 -0500

    clase 17/12/2025

--- modulo_4/mi-app-UTE/App.tsx
+++ modulo_4/mi-app-UTE/App.tsx
@@ -1,69 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { StateExample } from './src/screens/StateExample';
-import { AppButton } from './src/components/AppButton';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { Parte05Lab } from "./src/screens/Parte05Lab";
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Mi app ute</Text>
-      <Text style={styles.subtitle}>Entorno listo</Text>
-      <Text style={styles.text}>Kevin Cóndor</Text>
-      <StateExample/>
-      <AppButton
-        title="Presionar aquí"
-        onPress={() => console.log('Botón presionado!')}
-        variant="primary"
-      />
-      
-      <View style={{ height: 10 }} />
-      
-      <AppButton
-        title="Cargando..."
-        onPress={() => {}}
-        loading={true}
-        variant="secondary"
-      />
-      
-      <View style={{ height: 10 }} />
-      
-      <AppButton
-        title="Deshabilitado"
-        onPress={() => {}}
-        disabled={true}
-        variant="outline"
-      />
+    <>
+      <Parte05Lab />
       <StatusBar style="light" />
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000000ff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#0d1117",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 16,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: "600",
-    color: 'yellow',
-    marginBottom: 15
-  },
-  subtitle: {
+  texto: {
+    flex: 1,
+    color: "#ffffff",
+    backgroundColor: "#0d1117",
+    alignItems: "center",
+    justifyContent: "center",
     fontSize: 18,
-    fontWeight: "600",
-    marginBottom: 8,
-    color: "#c9d1d9",
   },
-  text: {
-    fontSize: 12,
-    fontWeight: "600",
-    marginBottom: 8,
-    color: "#c9d1d9",
-  },
-  light: {
-    color: "red"
-  }
 });
